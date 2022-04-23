8904bb4924a6cdc406a81083f66d405c31593ec3
hyc2026 <hyc2026@yeah.net>
2022-04-23 14:55:13 +0800

    add crnn

---
+++ crnn/pytorchocr/modeling/__init__.py
@@ -0,0 +1,7 @@
+"""
+@graduation project
+@School of Computer Science and Engineering, Beihang University
+@author  : hyc
+@File    : __init__.py
+@contact : hyc2026@yeah.net
+"""
---
+++ crnn/pytorchocr/modeling/heads/rec_ctc_head.py
@@ -0,0 +1,26 @@
+"""
+@graduation project
+@School of Computer Science and Engineering, Beihang University
+@author  : hyc
+@File    : rec_ctc_head.py
+@contact : hyc2026@yeah.net
+"""
+import os, sys
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class CTCHead(nn.Module):
+    def __init__(self, in_channels, out_channels=6625, fc_decay=0.0004, **kwargs):
+        super(CTCHead, self).__init__()
+        self.fc = nn.Linear(
+            in_channels,
+            out_channels,
+            bias=True,)
+        self.out_channels = out_channels
+
+    def forward(self, x, labels=None):
+        predicts = self.fc(x)
+        if not self.training:
+            predicts = F.softmax(predicts, dim=2)
+        return predicts
